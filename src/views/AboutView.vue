092447ef6165931fce2736570d5d64fe7e58f892
MirrorML <2867405686@qq.com>
2022-11-08 16:53:57 +0800

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="building">
     <div class="about">
-      <h1>This is an about page</h1>
+      <h1>SE331-2022-Project-Frontend</h1>
+      <h4>
+        Github Link:
+        https://github.com/SE331-Componet-Based-Software-Develop/SE331-2022-Project-Frontend.git
+      </h4>
+      <h4>Group Members:</h4>
+      <h4>632115516 Mingming Jia</h4>
+      <h4>632115518 Zhenxv Wang</h4>
     </div>
   </div>
 </template>
