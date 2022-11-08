43d2169b4d07a6a9b74391a486c0d69e09743bb7
MirrorML <2867405686@qq.com>
2022-11-08 17:09:10 +0800

    1

--- src/services/AxiosClient.js
+++ src/services/AxiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: process.env.VUE_APP_BACKEND_URL,
+  baseURL: 'http://54.91.217.177:8080',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
